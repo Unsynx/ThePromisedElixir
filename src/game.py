1743d6bcc59508fa166918fc468583431ffaf1f8
Nik <geeseflyomnidirectional@gmail.com>
2023-10-08 14:20:52 -0400

    Cleaned up game.py

--- src/game.py
+++ src/game.py
@@ -2,7 +2,6 @@ from tiles import Camera, TileManager, CHUNK_SIZE, TILE_SIZE
 from scene_manager import Scene, SceneManager
 from gui import GuiManager, Guide, Text, Button
 import pygame
-from gen import generate_dungeon
 from entity import Player, Enemy, EntityGroup
 
 
@@ -13,18 +12,18 @@ class GameScene(Scene):
 
         self.guiManager = GuiManager(self.screen)
 
-        # Debug Gui
+        # -------------- Debug GUI -------------- #
         self.debug = self.guiManager.add_guideline(
             Guide("center", None, Guide.GL_VERTICAL, 0, Guide.ALIGN_TOP, Guide.REL_ALIGN_RIGHT, 0))
         self.fps = self.debug.add_element(Text("", Text.FONT_BASE, Text.SIZE_HEADER, (255, 255, 255)))
         self.cam_pos = self.debug.add_element(Text("", Text.FONT_BASE, Text.SIZE_HEADER, (255, 255, 255)))
         self.plyr_pos = self.debug.add_element(Text("", Text.FONT_BASE, Text.SIZE_HEADER, (255, 255, 255)))
         self.plyr_tile_pos = self.debug.add_element(Text("", Text.FONT_BASE, Text.SIZE_HEADER, (255, 255, 255)))
-        # Doesn't work?
         self.reload = self.debug.add_element(Button("Re-Generate", 300, 80, self.sceneManager.set_scene, "loadingScreen", True))
 
         self.debug.hide = True
 
+        # -------------- Game Classes -------------- #
         # Set up camera and tile manager.
         self.camera = Camera(self.screen.get_size(), TILE_SIZE, None)
         self.camera.mode = Camera.CENTER_FIRST_ENTITY_SMOOTH
@@ -32,12 +31,14 @@ class GameScene(Scene):
         self.start_x, self.start_y = 0, 0
         self.tileManager = TileManager(self.screen, TILE_SIZE, CHUNK_SIZE, self.camera)
 
+        # -------------- Entities and Player -------------- #
         self.group = EntityGroup(self.camera, self.screen, self.tileManager, TILE_SIZE)
         self.camera.entity_group = self.group
         # Set up player.
         self.player = self.group.add_entity(Player)
         self.player.set_position(0, 0)
 
+        # -------------- TEMP: Debug only -------------- #
         # Temp: Create Enemy
         self.group.add_entity(Enemy).set_position(10, 10)
 
@@ -47,10 +48,11 @@ class GameScene(Scene):
 
         self.debug_cool = False
 
-    def on_scene_start(self, new):
-        if new:
+    def on_scene_start(self, is_loading_save):
+        if is_loading_save:
             self.group.load()
         else:
+            # Calls when world is newly generated
             self.player.set_position(self.start_x, self.start_y)
 
         self.player = self.group.entities[0]

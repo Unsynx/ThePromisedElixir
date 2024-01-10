5a646dbcb7d33b3f9c18b3de22966e536183c179
Nik <geeseflyomnidirectional@gmail.com>
2024-01-10 13:19:12 -0500

    Healhbar shows max health

--- src/game.py
+++ src/game.py
@@ -24,6 +24,7 @@ class GameScene(Scene):
         self.plyr_pos = self.debug.add_element(Text("", Text.FONT_BASE, Text.SIZE_HEADER, (255, 255, 255)))
         self.plyr_tile_pos = self.debug.add_element(Text("", Text.FONT_BASE, Text.SIZE_HEADER, (255, 255, 255)))
         self.weapon = self.debug.add_element(Text("", Text.FONT_BASE, Text.SIZE_HEADER, (255, 255, 255)))
+        self.lvl = self.debug.add_element(Text("", Text.FONT_BASE, Text.SIZE_HEADER, (255, 255, 255)))
 
         self.debug.hide = True
 
@@ -114,12 +115,13 @@ class GameScene(Scene):
         self.cam_pos.set_value(f"Camera: {round(self.camera.x, 0)}, {round(self.camera.y, 0)}")
         self.plyr_pos.set_value(f"Plyr - Position: {round(self.player.x, 0)}, {round(self.player.y, 0)}")
         self.plyr_tile_pos.set_value(f"Plyr - Tile Position: {self.player.tile_x}, {self.player.tile_y}")
-        self.weapon.set_value(f"Weapon: {type(self.player.weapon).__name__}")
+        self.weapon.set_value(f"Weapon: {self.player.weapon.name}")
+        self.lvl.set_value(f"Floor: {self.level}")
 
         # Player UI
         if self.player:
             self.health_bar.set_value(self.player.health / self.player.max_health)
-            self.weapon_attack.set_value(f"{self.player.weapon.name} - {self.player.weapon.damage}dmg")
+            self.weapon_attack.set_value(f"{self.player.health}/{self.player.max_health}hp       {self.player.weapon.name} - {self.player.weapon.damage}dmg")
 
     def render(self, screen: pygame.Surface):
         screen.fill((50, 60, 57))
